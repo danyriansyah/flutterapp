ad9e8ffee519aa49d2f978bd96f76f9f7ce1004d
danyriansyah <danyriansyahp@gmail.com>
2022-07-13 14:23:07 +0700

    add more detail page

--- coffeeshop/lib/screens/gold_brew.dart
+++ coffeeshop/lib/screens/gold_brew.dart
@@ -1,4 +1,8 @@
+import 'package:coffeeshop/screens/details.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:smooth_star_rating_null_safety/smooth_star_rating_null_safety.dart';
+import '../constants/color_palette.dart';
 
 class GoldBrew extends StatefulWidget {
   const GoldBrew({Key? key}) : super(key: key);
@@ -9,9 +13,174 @@ class GoldBrew extends StatefulWidget {
 
 class _GoldBrewState extends State<GoldBrew> {
   @override
+  Widget buildListItem(String imgPath, String price, Color bgColor) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (contex) =>
+                CoffeeDetails(imgPath: imgPath, headerColor: bgColor)));
+      },
+      child: Stack(children: [
+        Container(
+          height: 300.0,
+          width: 250.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25.0),
+            color: Colors.transparent,
+          ),
+        ),
+        Positioned(
+          top: 100.0,
+          child: Container(
+            height: 200.0,
+            width: 250.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25.0),
+              image: DecorationImage(
+                image: AssetImage('assets/doodle.png'),
+                fit: BoxFit.none,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 100.0,
+          child: Container(
+            height: 200.0,
+            width: 250.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25.0),
+              color: Colors.white.withOpacity(0.6),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 100.0,
+          child: Container(
+            height: 200.0,
+            width: 250.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25.0),
+              color: bgColor.withOpacity(0.6),
+            ),
+          ),
+        ),
+        Positioned(
+          right: 2.0,
+          child: Hero(
+            tag: imgPath,
+            child: Container(
+              height: 250.0,
+              width: 150.0,
+              child: Image(
+                image: AssetImage(imgPath),
+                fit: BoxFit.scaleDown,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+            top: 125.0,
+            left: 15.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Price',
+                  style: GoogleFonts.bigShouldersText(
+                    color: Color(0xFF23163D),
+                    fontSize: 20.0,
+                  ),
+                ),
+                Text(
+                  '\$' + price,
+                  style: GoogleFonts.bigShouldersText(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  'Grady\'s COLD BREW',
+                  style: GoogleFonts.bigShouldersText(
+                    color: Color(0xFF23163D),
+                    fontSize: 27.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 2.0,
+                ),
+                Container(
+                  width: 200.0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '65 reviews',
+                            style: GoogleFonts.bigShouldersText(
+                              color: Colors.white,
+                              fontSize: 15.0,
+                            ),
+                          ),
+                          SmoothStarRating(
+                            starCount: 5,
+                            size: 15.0,
+                            color: Colors.white,
+                            borderColor: Colors.white,
+                            rating: 3.0,
+                          )
+                        ],
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          width: 75.0,
+                          height: 30.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(7.0),
+                            color: Colors.white,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.add,
+                                size: 17.0,
+                              ),
+                              SizedBox(
+                                width: 5.0,
+                              ),
+                              Text(
+                                'Add',
+                                style: GoogleFonts.bigShouldersText(
+                                  color: Color(0xFF23163D),
+                                  fontSize: 15.0,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ))
+      ]),
+    );
+  }
+
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Gold Brew'),
+    return ListView(
+      children: [
+        buildListItem('assets/coffee4.png', '150', ColorPalette().secondSlice),
+        buildListItem('assets/coffee2.png', '200', ColorPalette().firstSlice),
+      ],
     );
   }
 }
